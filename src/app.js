87042d96d2df47508804472cc1faf2bc26667866
bostonrex <reynaldogoitia@gmail.com>
2023-02-20 22:18:48 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const divExcuse = document.querySelector("excuse");
+
+  let who = ["dog", "cat", "turtle", "bird"];
+  let what = ["ate", "pissed", "crushed", "broke"];
+  let What = ["my homework", "my car", "my shoe", "my bike"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoRandom = Math.floor(Math.random() * who.length);
+  let whatRandom = Math.floor(Math.random() * what.length);
+  let WhatRandom = Math.floor(Math.random() * What.length);
+  let whenRandom = Math.floor(Math.random() * when.length);
+
+  let excuse = `${who[whoRandom]} ${what[whatRandom]} ${What[WhatRandom]} ${when[whenRandom]}`;
+
+  let newElement = document.createElement("h1");
+  newElement.innerHTML = excuse;
+
+  let excuseContainer = document.querySelector("#excuse");
+
+  excuseContainer.appendChild(newElement);
 };
